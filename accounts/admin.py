8cfd326646d5cf018d16c75f7ac398b2935a34d1
Raghav <vahgarkhurana@gmail.com>
2016-12-27 22:34:52 +0530

    Changes Begin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Pinteam
+from .models import BaseUser, StudentUser, SchoolAdmin
+
 from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+admin.site.register(BaseUser)
+admin.site.register(StudentUser)
+admin.site.register(SchoolAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,32 @@
 from django.db import models
-from School.models import School
-from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser, PermissionsMixin
 )
+from phonenumber_field.modelfields import PhoneNumberField
+
 # Create your models here.
+class ManagerForUser(BaseUserManager):
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        self.is_staff = False;
+        return self._create_user(email, password, **extra_fields)
+
 class NewUserManager(BaseUserManager):
 
     use_in_migrations = True
@@ -23,6 +45,7 @@ class NewUserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_superuser', False)
+        self.is_staff = False;
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
@@ -32,61 +55,36 @@ class NewUserManager(BaseUserManager):
         return self._create_user(email, password, **extra_fields)
 
 
-class SuperManager(BaseUserManager):
-
-
-    use_in_migrations = True
-
-    def _create_user(self, email, password, **extra_fields):
-        """
-        Creates and saves a User with the given email and password.
-        """
-        if not email:
-            raise ValueError('The given email must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
-
-    def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
-
-
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_superuser', True)
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-        return self._create_user(email, password, **extra_fields)
-
-
-# USER STARTS HERE
-
-class BaseUSER(AbstractBaseUser, PermissionsMixin):
+class BaseUser(AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=50, null=True, blank=True)
-    last_name = models.CharField(max_length=50, null=True, blank=True)
-    is_active = models.BooleanField(default=True)
+    first_name = models.CharField(max_length=30 )
+    last_name = models.CharField(max_length=50, blank=True)
     is_staff = models.BooleanField(default=True)
 
     objects = NewUserManager()
 
     USERNAME_FIELD = 'email'
 
+    def get_full_name(self):
+        '''
+        Returns the first_name plus the last_name, with a space in between.
+        '''
+        return self.first_name
+
+    def get_short_name(self):
+        '''
+        Returns the short name for the user.
+        '''
+        return self.first_name
+
     def __str__(self):
         return self.email
 
-    def get_short_name(self):
-        return self.first_name
-
-
-class StudentUser(BaseUSER):
+class StudentUser(BaseUser):
 
     student_id = models.CharField(primary_key=True,max_length=10)
-    school = models.ForeignKey(School,blank=True,null=True)
+    # school = models.ForeignKey(School,blank=True,null=True)
     address = models.CharField(max_length=500, blank=True)
     city = models.CharField(max_length=50,blank=True, null=True)
     state = models.CharField(max_length=50, blank=True, null=True)
@@ -98,50 +96,34 @@ class StudentUser(BaseUSER):
     standard = models.CharField(max_length=2,blank=True, null=True)
     # bus_transport = models.ForeignKey(Bus,blank=True,null=True)
 
-    objects = NewUserManager()
+
+    objects = ManagerForUser()
 
     USERNAME_FIELD = 'student_id'
 
+    def get_full_name(self):
+        '''
+        Returns the first_name plus the last_name, with a space in between.
+        '''
+        return self.first_name
+
+    def get_short_name(self):
+        '''
+        Returns the short name for the user.
+        '''
+        return self.first_name
+
     def __str__(self):
-        return self.student_id
+        return self.email
 
-class SchoolAdmin(BaseUSER):
+class SchoolAdmin(BaseUser):
 
-    school = models.ForeignKey(School,blank=True,null=True)
+    # school = models.ForeignKey(School,blank=True,null=True)
 
-    objects = NewUserManager()
+    objects = ManagerForUser()
 
     USERNAME_FIELD = 'email'
 
     def __str__(self):
         x = self.email
         return x
-
-class Pinteam(BaseUSER):
-
-    pin_level = models.CharField(max_length=10)
-
-
-    def get_full_name(self):
-        return self.first_name
-
-    def get_short_name(self):
-        return self.first_name
-
-    def is_superuser(self):
-        return self.is_admin
-
-    def is_staff(self):
-        return self.is_admin
-
-    def has_perm(self, perm, obj=None):
-        return self.is_admin
-
-    def has_module_perms(self, app_label):
-        return self.is_admin
-
-
-
-    objects = SuperManager()
-
-    USERNAME_FIELD = 'email'
